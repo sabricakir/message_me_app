32b1c805e1dd34a68158d04dfe5c8f6eca0ba561
sabricakir <sabricakir86@gmail.com>
2022-07-12 05:48:07 +0300

    added restricts

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomController < ApplicationController
-
+    before_action :require_user, only: [:index]
+    
     def index
         @messages = Message.all.order("created_at ASC")
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-
+    before_action :logged_in_redirect, only: [:new, :create]
     def new
 
     end
@@ -21,5 +21,14 @@ class SessionsController < ApplicationController
         session[:user_id] = nil
         flash[:success] = "Başarıyla Çıkış Yaptınız"
         redirect_to login_path
+    end
+
+    private
+
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = "Zaten Giriş Yaptınız"
+            redirect_to root_path
+        end
     end
 end
